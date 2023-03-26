09ec7be41bec908a8b1b1a51eb0daa759680cc26
liranluxury <lirangraf@gmail.com>
2023-03-26 15:22:41 +0300

    more

--- yurafunct.js
+++ yurafunct.js
@@ -8,11 +8,51 @@ complexHello('Hi!', 'Iren', 46);
 complexHello('Hi!', 'Ars', 29);
 
 function square(number) {
-    console.log(number * number);
+    return number * number;
 }
-square(522);
+let y = square(522);
+console.log(y);
+
+function isSquareBig(side) {
+    const squareArea = square(side);
+    if (squareArea > 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isSquareBig(15));
+
 
 function totalArea(width, heigth) {
     console.log(width * heigth);
 }
 totalArea(12, 43);
+
+// function animalVoice(animal, animalName){
+//     switch(animal){
+//         case "dog":
+//             return animalName + " burks.";
+//             case "cat":
+//                 return animalName + " meows.";
+//                 case "pig":
+//                     return animalName + " grunts.";
+//                     default :
+//                     return animalName + " makes some sounds.";
+//     }
+// }
+// console.log(animalVoice("cat", "Richard"));
+
+const animalVoice = function(animal, animalName) {
+    switch(animal){
+        case "dog":
+            return animalName + " burks.";
+            case "cat":
+                return animalName + " meows.";
+                case "pig":
+                    return animalName + " grunts.";
+                    default :
+                    return animalName + " makes some sounds.";
+    } 
+}
+console.log(animalVoice("cat", "Richard"));
