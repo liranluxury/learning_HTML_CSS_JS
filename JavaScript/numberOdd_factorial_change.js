2a04d9cd66cfb7a146adf5c61a8e840459655b7c
liranluxury <lirangraf@gmail.com>
2023-03-29 10:30:50 +0300

    more

--- JavaScript/numberOdd_factorial_change.js
+++ JavaScript/numberOdd_factorial_change.js
@@ -6,7 +6,7 @@ function isNumberOdd (number) {
    }
 }
 console.log(isNumberOdd(9));
-
+'use strict';
 
 function factorial(count){
     let result = 1;
@@ -23,3 +23,48 @@ function changeSpaceToUnderscore(text){
     return resultText;
 }
 console.log(changeSpaceToUnderscore('My name is Liran'));
+
+let num = 50;
+while (num < 55){
+    console.log(num);
+    num++;
+}
+
+let num = 50;
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i=1; i<8; i++){
+    console.log(i);
+}
+
+
+let num = 50;
+for (let i=1; i<8; i++){
+    console.log(num);
+    num++;
+}
+
+
+for (let i=1; i<8; i++){
+    if (i=== 6){
+        break;
+    }
+    console.log(i);
+}
+
+for (let i=1; i<18; i++){
+    if (i=== 6 || i=== 12 || i=== 15){
+       continue;
+    }
+    console.log(i);
+}
+
+
+
+
+
+
