543c615f867ebf5bdecd70621f664ed9d0bad640
liranluxury <lirangraf@gmail.com>
2023-03-30 16:19:43 +0300

    more

---
+++ JavaScript/strings300323.js
@@ -0,0 +1,31 @@
+'use strict'
+let text = 'Helllllo!';
+for(const charrry of text){
+    console.log(charrry);
+}
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+for(let i=0; i< arr.length; i++){
+    result[i]=arr[i];
+} 
+console.log(result);
+
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+for(let i=0; i<data.length; i++){
+    if(typeof(data[i])=== 'number'){
+     data[i]=data[i]*2;
+    }
+         else if(typeof(data[i])==='string'){
+            data[i]=`${data[i]}-done`
+        }
+    }console.log(data);
+
+
+
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
