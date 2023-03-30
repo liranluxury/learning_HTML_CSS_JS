42149ebe7237c13040ac11efdf3fd95714f56ace
liranluxury <lirangraf@gmail.com>
2023-03-30 16:45:06 +0300

    more

--- JavaScript/strings300323.js
+++ JavaScript/strings300323.js
@@ -26,8 +26,35 @@ for(const charrry of text){
 
 
 
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-const result = [];
-for(let i=1; i<=data.length; i++){
-    result[i-1]=data[data.length-i];
-}console.log(result); 
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+// for(let i=1; i<=data.length; i++){
+//     result[i-1]=data[data.length-i];
+// }console.log(result); 
+
+
+// let result = '';
+// const length = 7;
+// for (let i = 1; i < length; i++){
+//     for (let j = 0; j < i; j++){
+//         result += "*";
+//     }
+//     result +='\n';
+// }
+
+
+// console.log(result);
+
+
+const lines = 5;
+let result = '';
+for(let i=0; i<=lines; i++){
+    for(let j=0; j<lines-i; j++){
+        result+=' ';
+    }
+    for(let j=0; j<2*i+1; j++){
+        result+='*';
+    }
+    result +='\n';
+}
+console.log(result);
