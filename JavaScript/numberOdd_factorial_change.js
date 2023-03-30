e145b37aa6da7a0475202d53a47c4b1303b0fab3
liranluxury <lirangraf@gmail.com>
2023-03-30 13:20:06 +0300

    more

--- JavaScript/numberOdd_factorial_change.js
+++ JavaScript/numberOdd_factorial_change.js
@@ -142,3 +142,13 @@ let a = 2
     return result;
 
 const data = [5, 10, 'Shopping', 20, 'Homework'];
+for  (let i = 0; i < data.length; i++){
+    if(typeof(data[i])=== 'number'){
+        data[i]= data[i]*2;
+    } else if(typeof(data[i])=== 'string'){
+        data[i] = `${data[i]} - done`;
+    }
+    }
+console.log(data);
+return data;
+
