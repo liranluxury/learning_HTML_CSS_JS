b696b2e69bf934de56ea096903c07cfe98503a03
liranluxury <lirangraf@gmail.com>
2023-04-02 10:31:52 +0300

    more

--- ifelse/objects020423.js
+++ ifelse/objects020423.js
@@ -28,9 +28,35 @@ const cars = [
 // if (sale === false){
 //     console.log(`Still on sale: ${car()}`)
 // }
-for (let i=0; i < cars.length; i++)
-{
-    if (cars[i].saled === false){
-        console.log(cars[i]);
+// for (let i=0; i < cars.length; i++)
+// {
+//     if (cars[i].saled === false){
+//         console.log(cars[i]);
+//     }
+// }
+ cars.forEach(function(carr){
+    if(carr.saled=== false){
+        console.log(carr);
     }
+ })
+
+
+
+ const carSeller = {
+    firstName: 'Jack',
+    lastName: 'White',
+    regYear: 2017,
+    hasDiscount : true,
+    discountCalculation: function(year){
+        let discount;
+        let numberOfYears = 2023 - year;
+        if(numberOfYears <= 2) {
+            discount = 0;
+        }
+     else if(numberOfYears > 5) {
+        discount = 30;
+    }
+    return discount;
+ }
 }
+console.log(carSeller.discountCalculation(2021));
