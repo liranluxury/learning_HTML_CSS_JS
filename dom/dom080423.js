06c2649d0c60a165ee0e110e070f8c85cbf8c79f
liranluxury <lirangraf@gmail.com>
2023-04-08 16:51:17 +0300

    more

--- dom/dom080423.js
+++ dom/dom080423.js
@@ -1,4 +1,8 @@
 document.getElementById("header").innerHTML = "What is the DOM?";
 let a = document.getElementsByClassName("firstThreeElements");
 console.log(a[1].innerHTML);
- console.log(document.getElementsByName("li")[1].innerHTML);
+console.log(document.getElementsByName("li")[1].innerHTML);
+console.log(document.getElementsByName("body")[0]);
+
+console.log(document.querySelector("#header").innerHTML);
+
