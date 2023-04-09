28d0630b7de6dfcca4b07c1393af622aa55e5c86
liranluxury <lirangraf@gmail.com>
2023-04-09 14:18:01 +0300

    more

--- dom/dom080423.js
+++ dom/dom080423.js
@@ -1,8 +1,17 @@
-document.getElementById("header").innerHTML = "What is the DOM?";
-let a = document.getElementsByClassName("firstThreeElements");
-console.log(a[1].innerHTML);
-console.log(document.getElementsByName("li")[1].innerHTML);
-console.log(document.getElementsByName("body")[0]);
+// document.getElementById("header").innerHTML = "What is the DOM?";
+// let a = document.getElementsByClassName("firstThreeElements");
+// console.log(a);
+// console.log(a[1].innerHTML);
+console.log(document.getElementsByTagName("li")[1].innerHTML);
+console.log(document.getElementsByTagName("li")[2].innerHTML);
+console.log(document.querySelectorAll(".firstThreeElements")[2]);
+// console.log(document.getElementsByTagName("body")[0]);
 
-console.log(document.querySelector("#header").innerHTML);
+// console.log(document.querySelector("#header").innerHTML);
+// document.querySelector("#header").innerHTML = "AAAAAAAAAAA";
+// console.log(document.querySelector("#header").innerHTML);
+// console.log(document.querySelectorAll(".firstThreeElements"));
 
+const li3 = document.getElementById("thirdElement");
+li3.style.color = "red";
+li3.style.fontSize = " 40px";
