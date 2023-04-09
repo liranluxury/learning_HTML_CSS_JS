c2c4ac6a2d0427f16ded642754ec692b4b1e1855
liranluxury <lirangraf@gmail.com>
2023-04-09 14:49:16 +0300

    more

--- dom/dom080423.js
+++ dom/dom080423.js
@@ -13,5 +13,16 @@ console.log(document.querySelectorAll(".firstThreeElements")[2]);
 // console.log(document.querySelectorAll(".firstThreeElements"));
 
 const li3 = document.getElementById("thirdElement");
-li3.style.color = "red";
-li3.style.fontSize = " 40px";
+// li3.style.color = "red";
+// li3.style.fontSize = " 40px";
+// li3.classList.add("stylish");
+li3.classList.toggle("stylish");
+
+const image = document.getElementsByTagName("img");
+image[0].getAttribute("src");
+image[0].setAttribute("src", "images/IMG_0042.jpg");
+
+const link = document.getElementsByTagName("a");
+link[0].getAttribute("href");
+link[0].setAttribute("href", "https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence");
+
