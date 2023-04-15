29808341b17f67208e6c0b5e6edb6dbc01776d6d
liranluxury <lirangraf@gmail.com>
2023-04-15 15:02:11 +0300

    more

--- dom/love110423.js
+++ dom/love110423.js
@@ -6,6 +6,14 @@ const secondName = prompt ("Write your name");
 
 const compatibility = (
     Math.floor(Math.random()*100)) 
+
+    if (compatibility < 33 && firstName != null && secondName != null && firstName != "" && secondName != ""){
+        alert (`As a result of compatibility ${firstName} and ${secondName} is ${compatibility} Try look for anohter name`);
+    } else if (compatibility >= 33 && compatibility < 66 && firstName != null && secondName != null && firstName != "" && secondName != ""){
+        alert (`As a result of compatibility ${firstName} and ${secondName} is ${compatibility} Compatibility is good, but will be better`);   
+    } else if (compatibility >= 66 && firstName != null && secondName != null && firstName != "" && secondName != ""){
+         alert (`As a result of compatibility ${firstName} and ${secondName} is ${compatibility} Compatibility is wonderfull!!`);   
+    } else {
+         alert (`Ups... Try again!`);   
+    }
             
-alert (`As a result of compatibility ${firstName} and ${secondName} is ${compatibility}`);
-console.log(compatibility);
