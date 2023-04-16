46a41260671913a64874210c8d2a9ccda76a267b
liranluxury <lirangraf@gmail.com>
2023-04-16 11:51:42 +0300

    more

--- dom/calc160423.js
+++ dom/calc160423.js
@@ -1,11 +1,21 @@
 'use strict';
 
 
-const resultCrazyKatz = (res1, res2, res3)=> (res1 + res2 + res3)/3;
-const resultFunnyDucks = (res1, res2, res3)=> (res1 + res2 + res3)/3;
+const calcAvarege = (res1, res2, res3)=> (res1 + res2 + res3)/3;
+ let crazyKats = calcAvarege (41, 23, 71);
+ let funnyDucks = calcAvarege ( 65, 154, 49);
 
-const averageResult = (resultCrazyKatz, resultFunnyDucks) => {
-    let avr = (resultCrazyKatz + resultFunnyDucks)/2;
-    return avr;
+
+
+function bigResult (crazyKats, funnyDucks){
+    if (crazyKats >= funnyDucks*2){
+        console.log(`Congratulation! CrazyKatz won!${crazyKats}`);
+    } else if (funnyDucks >= crazyKats*2){
+        console.log(`Congratulation! FunnyDucks won!${funnyDucks}`);
+    } else {
+        console.log("Congratulation! Zero draw!")
+    }
 }
+
+bigResult(crazyKats,funnyDucks);
 
