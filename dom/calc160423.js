4ac9473e76f8fbde32b2ece5543e590d2dc9c850
liranluxury <lirangraf@gmail.com>
2023-04-16 11:08:16 +0300

    more

---
+++ dom/calc160423.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+const resultCrazyKatz = (res1, res2, res3)=> (res1 + res2 + res3)/3;
+const resultFunnyDucks = (res1, res2, res3)=> (res1 + res2 + res3)/3;
+
+const averageResult = (resultCrazyKatz, resultFunnyDucks) => {
+    let avr = (resultCrazyKatz + resultFunnyDucks)/2;
+    return avr;
+}
+
